9cb8bcf51fbcf3d0e3ae03de7281587d68949141
roldanmoncada <roldan.h.moncada@gmail.com>
2023-02-15 16:37:42 -0700

    filled in the models and set up the framewor for all possible controller routes. Filling those in next.

---
+++ controllers/though-controller.js
@@ -0,0 +1,33 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getThoughts(req, res) {
+
+    },
+
+    getSingleThought(req, res) {
+
+    },
+
+    createThought(req, res) {
+
+    },
+
+    updateThought(req, res) {
+
+    },
+
+    deleteThought(req, res) {
+
+    },
+
+    addReaction(req, res) {
+
+    },
+
+    removeReaction(req, res) {
+        
+    }
+}
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,33 @@
+const { Thought, User } = require('../models');
+
+const userController = {
+    getUsers(req, res) {
+
+    },
+
+    getSingleUser(req, res) {
+
+    },
+
+    createUser(req, res) {
+
+    },
+
+    updateUser(req, res) {
+
+    },
+
+    deleteUser(req, res) {
+
+    },
+
+    addFriend(req, res) {
+
+    },
+
+    removeFriend(req, res) {
+        
+    }
+}
+
+module.exports = userController;
